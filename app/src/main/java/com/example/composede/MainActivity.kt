169b1be28cf318d311b4a708f8abeab6acefc398
superlative <6210612849@student.tu.ac.th>
2022-03-24 20:54:30 +0700

    original push

--- app/src/main/java/com/example/composede/MainActivity.kt
+++ app/src/main/java/com/example/composede/MainActivity.kt
@@ -44,10 +44,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-private  fun closeKeyBoard(view: View){
-    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE)as InputMethodManager
-    inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
-}
+
 
 @Composable
 fun Greeting(name: String) {
